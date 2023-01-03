d6352588f0cb3cb5f0ce893e3c3919dba81efedc
yellow_may <yellow.may.21@gmail.com>
2023-01-03 14:56:38 +0100

    modified wrapper

--- src/components/PersonalInfo.svelte
+++ src/components/PersonalInfo.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import FormWrapper from './layouts/FormWrapper.svelte';
+	import Wrapper from './layouts/Wrapper.svelte';
 	import { form_fields } from '../helpers';
+
+	let title = 'Personal Info';
+	let desc = 'Please provide your name, email address and phone number.';
 
 	export let back: any;
 	export let next: any;
+	export let stage: string;
 
 	$: errors = {};
 	form_fields.forEach(e => (errors = { ...errors, [e.id]: false }));
@@ -30,44 +34,35 @@
 			next();
 		}
 	};
-
-	export let stage: string;
 </script>
 
-<div>
-	<h1 class="font-bold text-2xl text-blue-300 mb-2">Personal Info</h1>
-	<p class="text-gray-100 tracking-wide mb-7">
-		Please provide your name, email address and phone number.
-	</p>
-
-	<FormWrapper {stage} {handle_submit} {back}>
-		<div class="space-y-5">
-			{#each form_fields as field}
-				<div class="space-y-1 text-sm text-blue-300">
-					<div class="flex items-center justify-between">
-						<label for={field.id} class="block">{field.label}</label>
-						{#if errors?.[field.id]}
-							<span class="text-red text-xs font-medium"
-								>This field is required</span
-							>
-						{/if}
-					</div>
-					<input
-						type={field.type}
-						placeholder={field.placeholder}
-						id={field.id}
-						name={field.id}
-						class={`border rounded border-gray-50 w-full p-3  ${
-							errors?.[field.id] ? 'outline-red' : ''
-						}`}
-						autocomplete="0"
-						on:input={e =>
-							e.currentTarget.value
-								? (errors = { ...errors, [field.id]: false })
-								: (errors = { ...errors, [field.id]: true })}
-					/>
+<Wrapper {title} {desc} {stage} {handle_submit} {back}>
+	<div class="space-y-5">
+		{#each form_fields as field}
+			<div class="space-y-1 text-sm text-blue-300">
+				<div class="flex items-center justify-between">
+					<label for={field.id} class="block">{field.label}</label>
+					{#if errors?.[field.id]}
+						<span class="text-red text-xs font-medium"
+							>This field is required</span
+						>
+					{/if}
 				</div>
-			{/each}
-		</div>
-	</FormWrapper>
-</div>
+				<input
+					type={field.type}
+					placeholder={field.placeholder}
+					id={field.id}
+					name={field.id}
+					class={`border rounded border-gray-50 w-full p-3  ${
+						errors?.[field.id] ? 'outline-red' : ''
+					}`}
+					autocomplete="0"
+					on:input={e =>
+						e.currentTarget.value
+							? (errors = { ...errors, [field.id]: false })
+							: (errors = { ...errors, [field.id]: true })}
+				/>
+			</div>
+		{/each}
+	</div>
+</Wrapper>
---
+++ src/components/layouts/Wrapper.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import ButtonGroup from '../UI/ButtonGroup.svelte';
+
+	export let title: string;
+	export let desc: string;
+	export let handle_submit: any;
+	export let stage: string;
+	export let back: any;
+</script>
+
+<div>
+	<h1 class="font-bold text-2xl text-blue-300 mb-2">{title}</h1>
+	<p class="text-gray-100 tracking-wide mb-7">
+		{desc}
+	</p>
+
+	<form on:submit|preventDefault={handle_submit}>
+		<slot />
+
+		<ButtonGroup {stage} {back} />
+	</form>
+</div>
